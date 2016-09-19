3e815fc3f76741ed96657cd294da0f07ca9c556d
tianez <saber_tz@163.com>
2016-09-19 19:55:57 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -9,65 +9,67 @@ var concat = require('gulp-concat') //文件合并
 var rename = require('gulp-rename') //文件更名
 var webpack = require("gulp-webpack")
 
-var dir = './resources/front/'
-var diro = './public/front/'
 
-var webpackConfig = require('./resources/' + dir + '/webpack.config.js')
+var dir = 'chat'
+var dirin = './resources/' + dir + '/'
+var out = './public/front/' + dir + '/'
 
-gulp.task("webpack", function () {
-    return gulp.src(dir + 'app.js')
+var webpackConfig = require(dirin + 'webpack.config.js')
+
+gulp.task("webpack", function() {
+    return gulp.src(dirin + 'app.js')
         .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest(diro + 'js'))
+        .pipe(gulp.dest(out + 'js'))
         .pipe(connect.reload())
 })
 
-gulp.task('min-css', function () {
-    gulp.src([diro + 'css/*.css', '!' + diro + 'css/*.min.css'])
+gulp.task('min-css', function() {
+    gulp.src([out + 'css/*.css', '!' + out + 'css/*.min.css'])
         .pipe(cssmin({
             compatibility: 'ie7' //兼容IE7及以下需设置compatibility属性
         }))
         .pipe(rename({
             suffix: ".min",
         }))
-        .pipe(gulp.dest(diro + 'css'))
+        .pipe(gulp.dest(out + 'css'))
 })
 
-gulp.task('min-js', function () {
-    return gulp.src([diro + 'js/*.js', '!' + diro + 'js/*.min.js'])
+gulp.task('min-js', function() {
+    return gulp.src([out + 'js/*.js', '!' + out + 'js/*.min.js'])
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(uglify())
-        .pipe(gulp.dest(diro + 'js'))
+        .pipe(gulp.dest(out + 'js'))
 })
 
-gulp.task('less', function () {
-    gulp.src(dir + 'less/*.less')
+gulp.task('less', function() {
+    gulp.src(dirin + 'less/*.less')
         .pipe(less())
-        .pipe(gulp.dest(diro + 'css'))
+        .pipe(gulp.dest(out + 'css'))
         .pipe(connect.reload())
 })
 
-gulp.task('web', function (cb) {
+gulp.task('web', function(cb) {
     runSequence('webpack', 'less', ['min-css', 'min-js'], cb);
 });
 
-gulp.task('connect', function () {
+gulp.task('connect', function() {
     connect.server({
         port: 3000,
         livereload: true
     })
 })
 
-gulp.task('watch', function () {
-    gulp.watch([dir + 'less/*.less', dir + 'components/forms/*.less'], ['less'])
-    gulp.watch(['app/html/*', 'app/layout/*.html'], ['html'])
+gulp.task('watch', function() {
+    gulp.watch([dirin + 'less/*.less', dirin + 'components/forms/*.less'], ['less'])
+    // gulp.watch(['app/html/*', 'app/layout/*.html'], ['html'])
     gulp.watch([
-        dir + 'app.js',
-        dir + 'global.js',
-        dir + 'pages/*.js',
-        dir + 'layout/*.js',
-        dir + 'components/**/*.js'
+        dirin + 'app.js',
+        dirin + 'global.js',
+        dirin + 'pages/*.js',
+        dirin + 'layout/*.js',
+        dirin + 'components/**/*.js'
     ], ['webpack'])
 })
 
